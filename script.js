e1dd7b98a8c98a79c242fc3d5dc77a6d45808b96
zsk549 <161069227+zsk549@users.noreply.github.com>
2024-08-02 17:28:32 -0300

    school updt

--- script.js
+++ script.js
@@ -14,11 +14,6 @@ setInterval(()=>{
     container[c].classList.add(`anime${c}`)
     artic[c].classList.add(`opacit`)
     c++
-//     setTimeout(()=>{
-//     container[c].classList.remove(`anime${c}`)
-//     artic[c].classList.remove(`opacit`)
-//     c=c+1
-// },9800);
 },10000);
 
 breakdownButton.forEach(function(btn, ind) {
@@ -30,58 +25,4 @@ breakdownButton.forEach(function(btn, ind) {
     // setTimeout(()=>this.classList.remove(`anime${ind}`),10000)
   });
 });
-    
 
-// setInterval(()=>{
-//     container[c+1].classList.remove('.anime'+(c+1))
-//     container[c-1].classList.remove('.anime'+(c-1))
-//     container[1].classList.add('.anime1')
-//     c++
-//     if(c==3) c=0;
-// }, 4000);
-// window.sr = ScrollReveal({ reset: false });
-
-// sr.reveal('.container', { duration: 4000});
-
-// sr.reveal('.wrapper', { duration: 5000});
-
-// sr.reveal('.container2', { duration: 1000});
-
-// let boxW = document.getElementById('raphaB');
-let rapha = document.getElementById('rapha');
-rapha.onmouseover = () =>{
-    rapha.src = 'imagens/umineko_2.png'
-}
-rapha.onmouseout = () => {
-    rapha.src = 'imagens/umineko.png'
-}
-
-//     let varRadomN;
-//     varRadomN = Math.floor(Math.random() * 50000) + 1
-let igor = document.getElementById('igor');
-igor.onmouseover = () =>{
-      igor.src = 'imagens/Aqua.png'
-  }
-igor.onmouseout = () => {
-      igor.src = 'imagens/Eugeo.png'
-  }
-let churras = document.getElementById('churras');
-let churrasko = document.querySelector('.boxK')
-churrasko.onmouseover = () =>{
-      churras.src = 'imagens/emiC.png'
-  }
-churrasko.onmouseout = () => {
-      churras.src = 'imagens/emi.png'
-  }
-//     setInterval(timeF, varRadomN)
-//     setInterval(timeF2, 400)
-
-//     function timeF(){
-//         igor.src = 'imagens/Aqua.png';
-        
-//     }
-
-//     function timeF2(){
-//         igor.src = 'imagens/Eugeo.png';
-        
-//     }
